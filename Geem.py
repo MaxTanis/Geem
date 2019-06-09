aaaa5d1fb3cbbc2385b5047fa70f2b55b9c95c5c
Max Tanis <110074@st-maartenscollege.nl>
2019-06-09 14:59:54 +0200

    begonnen met de introductie

--- Geem.py
+++ Geem.py
@@ -1,1 +1,9 @@
-print("Hello world!")
+player_location = "start_room"
+
+
+
+def introduction ():
+    name = input("What is your name?: ")
+    print("Hello" , name , "you are standing in an abandoned house.")
+
+introduction()
